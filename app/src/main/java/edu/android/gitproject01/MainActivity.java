2f329881cd78dcd812c862e5c26a60dfed7139cd
JulieTara <0427pjy@naver.com>
2018-04-13 14:04:32 +0900

    버튼을 지역변수로 바꿈

--- app/src/main/java/edu/android/gitproject01/MainActivity.java
+++ app/src/main/java/edu/android/gitproject01/MainActivity.java
@@ -8,14 +8,13 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     
     private TextView textView;
-    private Button button ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         int i;
-        button = findViewById(R.id.button);
+        Button button = findViewById(R.id.button);
         textView = findViewById(R.id.textView);
     }
 }
